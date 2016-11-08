c95a3d4a9eb6c3c023c974a22b56357a3ad122aa
Ken Ling <lingken.thu@icloud.com>
2016-11-07 19:05:35 -0500

    display image

--- nodejs/app/routes.js
+++ nodejs/app/routes.js
@@ -603,7 +603,7 @@ module.exports = function(app, passport) {
                         Description: row.Description});
                 });
 
-                serverDB.all('select DeviceID, DeviceName, Image, Description from Device where UserID = ?', userID, function(err, deviceRows) {
+                serverDB.all('select DeviceID, DeviceType, DeviceName, Image, Description from Device where UserID = ?', userID, function(err, deviceRows) {
                     if (err) {
                         res.render('profile.ejs', {
                             UserID: userID,
@@ -615,10 +615,18 @@ module.exports = function(app, passport) {
                         return;
                     }
                     deviceRows.forEach(function(row) {
+                        var image;
+                        if (row.DeviceType == 'Video') {
+                            image = 'http://i.kinja-img.com/gawker-media/image/upload/t_original/dckiksbsuyomwbs1paqs.png';
+                        } else if (row.DeviceType == 'Audio') {
+                            image = 'http://assets.store.hp.com/hpusstore/images/3pp_573X430/ge5916.png';
+                        } else {
+                            image = 'http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png';
+                        }
                         deviceList.push({
                             DeviceID: row.DeviceID,
                             DeviceName: row.DeviceName,
-                            Image: row.Image,
+                            Image: image,
                             Description: row.Description});
                     });
 
