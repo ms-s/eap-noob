190c63c5b49fbf0655c6a8664354c14356449ab0
Ken Ling <lingken.thu@icloud.com>
2016-12-06 00:48:13 -0500

    script for adding software update notification

---
+++ nodejs/software_update.js
@@ -0,0 +1,30 @@
+var serverDBPath = '/var/serverDB';
+
+serverDB = new sqlite3.Database(serverDBPath);
+serverDB.run(
+	'select DeviceID, DeviceName, Description from Device',
+	function(err, deviceRows) {
+		if (!err) {
+			deviceRows.forEach(function(row) {
+				var deviceID = parseInt(row.DeviceID);
+				var deviceName = row.DeviceName;
+				var description = row.Description;
+				if (description != '') {
+					serverDB.run(
+						'insert into Notification (DeviceID, NotificationType, Description)\
+						values(?, ?, ?)',
+						deviceID, 'SoftwareUpdate', 'Software Update for ' + deviceName,
+						function(err) {
+							if (err) {
+								console.log('Error when inserting to Notification: ' + err);
+							}
+						}
+					);
+				}
+			});
+		} else {
+			console.log('Error in select: ' + err);
+		}
+	}
+);
+serverDB.close();
